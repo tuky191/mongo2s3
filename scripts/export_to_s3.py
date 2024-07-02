01cbff171aa31ace7b0f0c0da11ce01cc49215c8
tuky191 <michal@terra.money>
2024-07-02 17:31:08 +0200

    use env variables for setup

--- scripts/export_to_s3.py
+++ scripts/export_to_s3.py
@@ -1,26 +1,31 @@
-
+import os
 import pymongo
 import boto3
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-import os
 import time
 
-# Set up your DocumentDB connection
-client = pymongo.MongoClient('your_documentdb_cluster_endpoint', username='your_username', password='your_password', ssl=True)
-db = client['your_database']
-collection = db['your_collection']
+# Set up your DocumentDB connection using environment variables
+mongodb_uri = os.environ['MONGODB_URI']
+mongodb_username = os.environ['MONGODB_USERNAME']
+mongodb_password = os.environ['MONGODB_PASSWORD']
+database_name = os.environ['DATABASE_NAME']
+collection_name = os.environ['COLLECTION_NAME']
 
-# Set up your S3 connection
+client = pymongo.MongoClient(mongodb_uri, username=mongodb_username, password=mongodb_password, ssl=True)
+db = client[database_name]
+collection = db[collection_name]
+
+# Set up your S3 connection using environment variables
 s3 = boto3.client('s3')
-bucket_name = 'your_s3_bucket_name'
-s3_prefix = 'your_s3_prefix/'  # Optional, for organizing files
+bucket_name = os.environ['S3_BUCKET_NAME']
+s3_prefix = os.environ.get('S3_PREFIX', '')  # Optional, for organizing files
 
 # Define the size of each chunk to export (in documents)
-chunk_size = 1000
+chunk_size = int(os.environ.get('CHUNK_SIZE', 1000))  # Default to 1000 if not set
 checkpoint_file = 'checkpoint.txt'
-max_retries = 5
+max_retries = int(os.environ.get('MAX_RETRIES', 5))  # Default to 5 if not set
 
 def export_to_s3(file_path, chunk_number):
     file_name = f'{s3_prefix}documentdb_export_chunk_{chunk_number}.parquet'
